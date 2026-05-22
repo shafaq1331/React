e837a036cb2f49c168c9e2f37d5a15892bbea0e1
shafaq1331 <shafaqnadeem61@gmail.com>
2026-05-22 20:20:10 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,10 +16,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 async function connectDB() {
     try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(MONGODB_URI);
         console.log("MongoDB connected!");
     } catch (err) {
         console.error("MongoDB connection error:", err);
